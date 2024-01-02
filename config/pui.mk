33bce5d06d2be9f5edec833c1a4721a261c66524
John Galt <johngaltfirstrun@gmail.com>
2024-01-02 11:36:34 -0500

    vendor: Add and update preinstalled package list to stop auto cloning
    Only extra adds needed, gms now ships it's own lists for this.
    
    Change-Id: Ib38cd40d81f3fd9dc2fbcfb99f25176f3bc8ddba
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/pui.mk
+++ config/pui.mk
@@ -15,3 +15,7 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml
