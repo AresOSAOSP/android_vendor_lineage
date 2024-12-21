553b7e525cf6d49ba01ab237589b36bc543c9499
priiii08918 <priyanshusinghal0818@proton.me>
2024-12-21 05:47:03 +0000

    vendor: Drop updater
    Change-Id: I5a210e66f93e005635c01acb73e4de4250ba932f

--- config/common.mk
+++ config/common.mk
@@ -138,11 +138,7 @@ PRODUCT_PACKAGES += \
 endif
 
 PRODUCT_PACKAGES += \
-    LineageSettingsProvider \
-    Updater
-
-PRODUCT_COPY_FILES += \
-    vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
+    LineageSettingsProvider
 
 # Charger
 PRODUCT_PACKAGES += \
